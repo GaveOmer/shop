c9c7a0b9d92adae2fce92393d045f0395c303d3c
feitan <ilya.shkoda06@gmail.com>
2024-01-09 04:39:27 +0300

    permissions detail-view

--- backend/main/views.py
+++ backend/main/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics, permissions
+from . import serializers
+from . import models
+
+
+class VendorList(generics.ListCreateAPIView):
+  queryset = models.Vendor.objects.all()
+  serializer_class = serializers.VendorSerializer
+  #permission_classes = [permissions.IsAuthenticated]
+  
+class VendorDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = models.Vendor.objects.all()
+  serializer_class = serializers.VendorSerializer
+  #permission_classes = [permissions.IsAuthenticated]
